61d0478c457ab09d9ef956fc6df33b4117b21758
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-02-01 10:45:29 +0100

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: 'next' },
+    ],
+  },
+}
