2909e66811f061f874d1e7a74c5ce9b331250095
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-02-03 07:54:26 +0100

    chore: refactor output file

--- packages/nats-services/rollup.config.js
+++ packages/nats-services/rollup.config.js
@@ -1,7 +1,7 @@
 import typescriptPlugin from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
-const deps = Object.keys(Object.assign({}, pkg.devDependencies))
+const deps = Object.keys(Object.assign({}, pkg.dependencies))
 
 const plugins = [typescriptPlugin()]
 
@@ -10,8 +10,8 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: pkg.module,
-        format: 'es',
+        file: pkg.main,
+        format: 'cjs',
         sourcemap: true,
       },
     ],
