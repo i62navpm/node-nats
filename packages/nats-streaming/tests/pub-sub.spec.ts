c618b0d14eafd612cd397988ae3f4b13007ff658
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-02-12 08:08:33 +0100

    chore: prepare test suite for streaming server

---
+++ packages/nats-streaming/tests/pub-sub.spec.ts
@@ -0,0 +1,42 @@
+import { Stan } from 'node-nats-streaming';
+import getConnection from '../src/index';
+
+describe("Test 'Publish-Subscribe' streaming", () => {
+  let stan: Stan;
+
+  beforeAll(() => {
+    try {
+      stan = getConnection();
+    } catch (err) {
+      throw new Error('NATS streaming server is disconnected');
+    }
+  });
+
+  it('NATS streaming server is connected', () => {
+    stan.on('connect', nc => expect(nc).toBeTruthy());
+  });
+
+  it('When a message is published a subscriber receive the message', () => {
+    return;
+  });
+
+  it('When a message is published multiple subscriber receive the message', () => {
+    return;
+  });
+
+  it('When a message is published only subjected subscribers will receive the message', () => {
+    return;
+  });
+
+  it('When a message is published only subjected with wildcard "*" subscribers will receive the message', () => {
+    return;
+  });
+
+  it('When a message is published only subjected with wildcard ">" subscribers will receive the message', () => {
+    return;
+  });
+
+  afterAll(() => {
+    stan.close();
+  });
+});
